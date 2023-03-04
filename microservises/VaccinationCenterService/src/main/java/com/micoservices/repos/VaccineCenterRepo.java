2c90b917d30c70d80879cee312d721b226170ea6
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-03-04 23:35:54 +0530

    Delete microservises directory

--- microservises/VaccinationCenterService/src/main/java/com/micoservices/repos/VaccineCenterRepo.java
+++
@@ -1,9 +0,0 @@
-package com.micoservices.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.micoservices.entities.VaccineCenter;
-
-public interface VaccineCenterRepo extends JpaRepository<VaccineCenter, Integer>{
-
-}
