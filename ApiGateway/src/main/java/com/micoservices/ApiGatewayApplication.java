f52e715ffa127949f4834f110d3852625934c72a
Pritu2022 <candlepritu21century@gmail.com>
2023-03-04 23:43:26 +0530

    eureka, hystrix, api gateway completed

---
+++ ApiGateway/src/main/java/com/micoservices/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.micoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+		
+		System.out.println("api gateway");
+	}
+
+}
